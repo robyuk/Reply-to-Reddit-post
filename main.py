d6268ad4a13f8d4966755a8cab5003389a6eb110
robertay <94378311+robyuk@users.noreply.github.com>
2022-11-08 17:22:39 +0000

    changedc the subreddit to glassblowing and now replies to Christmas posts

--- main.py
+++ main.py
@@ -21,8 +21,13 @@
 
 import praw   # External module for reddit
 from os import getenv
+from time import time
+from datetime import timedelta
 
-subreddit_name='pythonsandlot'
+recent=int(timedelta(hours=24).total_seconds())
+#print(type(recent),recent)
+
+subreddit_name='glassblowing'
 
 # Create an instance of the reddit class with your reddit account details
 reddit = praw.Reddit(user_agent=True, 
@@ -35,12 +40,18 @@ reddit = praw.Reddit(user_agent=True,
 subreddit = reddit.subreddit(subreddit_name)
 subreddit.validate_on_submit=True   # Avoids a deprecation warning
 
-# Create title and content of the post
-title="This is my first post using python!"
-content="""
-Hey, I am trying out python posts
-Lets see if this works!
-"""
+now = time()
 
-# Submit the post
-subreddit.submit(title=title, selftext=content)
+# Iterate through the recent posts in this subreddit
+for post in subreddit.new():
+  if now - post.created < recent:  # post created in the last recent time
+    if "christmas" in post.title.lower():
+      print(f'replying to post: {post.title}')
+      post.reply('Merry Christmas!')  # Reply to post
+
+      # We can also iterate through the comments to a post and reply to comments:
+      for comment in post.comments:
+        if "christmas is coming" in comment.body.lower():
+          comment.reply("Yeah, it's coming!")
+          pass
+  
